81c61cbe055fba69bbfbf3358c7a070c5dc0b492
Krishna Manoj PVR <pvrkmsbunny@gmail.com>
2024-02-02 22:02:48 +0530

    QueueArray

--- 46_Queue_Array.cpp
+++ 46_Queue_Array.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
-#define N 5
+#define N 2
 int arr[N];
 int front = -1;
 int rear = -1;
 void en(int x){
-    if(front==1 && rear==N-1){
+    if(rear==N-1){
         cout<<"Overflow"<<endl;
     }
     else if(front==-1 && rear==-1){
